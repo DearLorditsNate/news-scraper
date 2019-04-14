f124ebfd4041fc58b8cfda5bd391b3b5c4dcf410
Nate Micinski <nmicinski@gmail.com>
2019-04-14 15:09:04 -0700

    refactor routing architecture, remove controllers

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Dependencies
 var express = require("express");
 var mongoose = require("mongoose");
-// var axios = require("axios");
-// var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
 // var db = require("/models");
 
 // Express
@@ -23,10 +24,14 @@ app.use(express.static("public"));
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
 
-// Require controller and use routes
-var routes = require("./controllers/burgers_controller.js");
+// Routes
+app.get("/", function(req, res) {
+    res.render("home");
+});
 
-app.use(routes);
+app.get("/saved", function (req, res) {
+    res.render("saved");
+});
 
 // Start the server
 app.listen(PORT, function () {
